041d136ff81bce29994c1f69228bdcd62ac0c858
kouichi <kouichiume@gmail.com>
2018-04-17 01:11:01 +0900

    inside the steps

--- jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
+++ jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
@@ -6,9 +6,8 @@ pipeline {
 				checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [
 						[credentialsId: '7aebbc8e-9777-437a-9290-e93f577e4da8', url: 'https://github.com/sharebase/sharecoin-web.git']
 					]])
+				sh 'mvn'
 			}
-
-			steps { sh 'mvn' }
 		}
 		stage('Test'){
 			steps {
@@ -16,7 +15,6 @@ pipeline {
 				junit 'reports/**/*.xml'
 			}
 		}
-		stage('Deploy') { steps { sh 'make publish'
-			} }
+		stage('Deploy') { steps { sh 'make publish' } }
 	}
 }
