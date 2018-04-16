29b1e89ea438006c782cf6325199aa523e224560
kouichi <kouichiume@gmail.com>
2018-04-16 22:22:35 +0900

    from sample

--- jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
+++ jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
@@ -1,6 +1,19 @@
-package nagoya.sroi
-
+pipeline {
+	agent any
+	stages {
+		stage('Build') {
+			steps { sh 'make' }
+		}
+		stage('Test'){
+			steps {
+				sh 'make check'
+				junit 'reports/**/*.xml'
+			}
+		}
+		stage('Deploy') {
+			steps { sh 'make publish' }
+		}
+	}
+}
 class JenkinsStartScript {
-
-
 }
