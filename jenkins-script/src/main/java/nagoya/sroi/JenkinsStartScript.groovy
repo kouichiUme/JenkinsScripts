282421e9317301b62a598ce8aa1c6b181d1fe3ba
Kouichi Umezawa <kouichiume@gmail.com>
2021-04-12 22:34:42 +0900

    add ff pga project

--- jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
+++ jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
@@ -18,6 +18,11 @@ pipeline {
 						[credentialsId: '7aebbc8e-9777-437a-9290-e93f577e4da8', url: 'https://github.com/kouichiUme/AndroidNativeSample.git']
 					]])
 				sh './gradlew build'
+				checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [
+					[credentialsId: '7aebbc8e-9777-437a-9290-e93f577e4da8', url: 'https://github.com/kouichiUme/fpgaPractiseProject.git']
+				]])
+				sh ". /tools/Xilinx/Vitis_HLS/2020.2/bin/setupEnv.sh"
+				
 			}
 		}
 		stage('Test'){
