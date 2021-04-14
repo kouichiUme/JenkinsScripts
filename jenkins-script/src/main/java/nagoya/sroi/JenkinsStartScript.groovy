c0892c5b7eda7f94461a0caa46053375e870133c
Kouichi Umezawa <kouichiume@gmail.com>
2021-04-14 22:45:17 +0900

    2min code

--- jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
+++ jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
@@ -22,6 +22,7 @@ pipeline {
 					[credentialsId: 'github', url: 'https://github.com/kouichiUme/fpgaPractiseProject.git']
 				]])
 				sh ". /tools/Xilinx/Vitis_HLS/2020.2/bin/setupEnv.sh"
+				sh ". /tools/Xilinx/Vitis/2020.2/settings64.sh"
 				
 			}
 		}
