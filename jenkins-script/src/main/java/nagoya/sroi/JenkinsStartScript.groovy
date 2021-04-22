842badd1521ce90f66576042612739d5aeed14ec
Kouichi Umezawa <kouichiume@gmail.com>
2021-04-22 23:25:06 +0900

    android dir and serverside

--- jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
+++ jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
@@ -10,11 +10,13 @@ pipeline {
 	stages {
 	stage('Build') {
 			steps {
-				dir('blockchain')
-				checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [
-						[credentialsId: '7aebbc8e-9777-437a-9290-e93f577e4da8', url: 'https://github.com/sharebase/sharecoin-web.git']
-					]])
-				sh 'mvn compile'
+				dir('blockchain'){
+					checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [
+							[credentialsId: '7aebbc8e-9777-437a-9290-e93f577e4da8', url: 'https://github.com/sharebase/sharecoin-web.git']
+						]])
+					sh 'mvn compile'
+
+				}
 				dir('android'){
 					checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [
 							[credentialsId: '7aebbc8e-9777-437a-9290-e93f577e4da8', url: 'https://github.com/kouichiUme/AndroidNativeSample.git']
