6eb62e75a51722f490c97fca35558c6912b1af6e
Kouichi Umezawa <kouichiume@gmail.com>
2021-04-22 23:23:22 +0900

    android dir

--- jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
+++ jenkins-script/src/main/java/nagoya/sroi/JenkinsStartScript.groovy
@@ -10,19 +10,22 @@ pipeline {
 	stages {
 	stage('Build') {
 			steps {
+				dir('blockchain')
 				checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [
 						[credentialsId: '7aebbc8e-9777-437a-9290-e93f577e4da8', url: 'https://github.com/sharebase/sharecoin-web.git']
 					]])
 				sh 'mvn compile'
-				checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [
-						[credentialsId: '7aebbc8e-9777-437a-9290-e93f577e4da8', url: 'https://github.com/kouichiUme/AndroidNativeSample.git']
-					]])
-				sh '''
-				export ANDROID_SDK_ROOT=/home/kouichi/Android/Sdk
-				export NDK_PROJECT_PATH=.
-				 ./gradlew build
-				${ANDROID_SDK_ROOT}/cmake/3.10.2.4988404/bin/cmake
-				'''
+				dir('android'){
+					checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [
+							[credentialsId: '7aebbc8e-9777-437a-9290-e93f577e4da8', url: 'https://github.com/kouichiUme/AndroidNativeSample.git']
+						]])
+					sh '''
+					export ANDROID_SDK_ROOT=/home/kouichi/Android/Sdk
+					export NDK_PROJECT_PATH=.
+					./gradlew build
+					${ANDROID_SDK_ROOT}/cmake/3.10.2.4988404/bin/cmake
+					'''
+				}
 				dir('fpga'){
 					// checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [
 					// 	[credentialsId: 'github', url: 'https://github.com/kouichiUme/fpgaPractiseProject.git']
@@ -53,7 +56,7 @@ pipeline {
 			 steps {
 				echo "start deploying "
 				echo map.length
-				sh ". /tools/Xilinx/Vitis_HLS/2020.2/bin/setupEnv.sh"
+				// sh ". /tools/Xilinx/Vitis_HLS/2020.2/bin/setupEnv.sh"
 				sh "env"
 				
 				// echo map
