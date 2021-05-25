04f9aa7a561c86ced07cec85296cb9f42284b8d1
Kouichi Umezawa <kouichiume@gmail.com>
2021-05-25 23:00:30 +0900

    build win aspdotnet

--- jenkins-script/src/main/java/nagoya/sroi/buildOnWindowsScript.groovy
+++ jenkins-script/src/main/java/nagoya/sroi/buildOnWindowsScript.groovy
@@ -12,6 +12,7 @@ pipeline {
 			steps {
 				bat "nuget restore "path to *.sln" file"
 				bat "dotnet build YourProjectPath\\Project.csproj --configuration Release"				
+				bat "dotnet publish -c Release --no-build"
 				bat "msbuild "
 			}
 		}
@@ -24,7 +25,10 @@ pipeline {
 		stage('Deploy') {
 			 steps {
 				echo "start deploying "
-				
+			 	withCredentials([usernamePassword(credentialsId: 'iis-credential', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
+				 {
+					  bat """ "C:\\Program Files (x86)\\IIS\\Microsoft Web Deploy V3\\msdeploy.exe" -verb:sync -source:iisApp="${WORKSPACE}\\${publishedPath}" -enableRule:AppOffline -dest:iisApp="${iisApplicationName}",ComputerName="https://${targetServerIP}:8172/msdeploy.axd",UserName="$USERNAME",Password="$PASSWORD",AuthType="Basic" -allowUntrusted"""
+				}
 				// echo map
 			 
 			 }
